3e835cdd4da7961237126e3e5ff1b95044670b1a
Jacob Allen <jahocob@gmail.com>
2015-10-11 19:16:42 -0700

    Update instruction.cpp

--- instruction.cpp
+++ instruction.cpp
@@ -1,8 +1,18 @@
+/* instruction.cpp 
+   Authors : Lauren Heinrichs, Brian Caloz, Jacob Allen
+   description : 
+   implementations of all the functions to grab specific parts
+   of a mips instruction statement and has the to_string function
+   which tests and converts the input into a understandable english
+   form.
+*/
+
 #include <iostream>
 #include "instruction.hpp"
 
 using namespace std;
 
+//grab opcode 
 bitset<6> Instruction:: opcode()
 {
   bitset<6> temp;
@@ -14,6 +24,7 @@ bitset<6> Instruction:: opcode()
   return temp;
 }
 
+//grab rs
 bitset<5> Instruction:: rs()
 {
   bitset<5> temp;
@@ -26,6 +37,7 @@ bitset<5> Instruction:: rs()
   return temp;
 }
 
+//grab rt
 bitset<5> Instruction:: rt()
 {
   bitset<5> temp;
@@ -38,6 +50,7 @@ bitset<5> Instruction:: rt()
   return temp;
 }
 
+//grab rd
 bitset<5> Instruction:: rd()
 {
   bitset<5> temp;
@@ -50,6 +63,7 @@ bitset<5> Instruction:: rd()
   return temp;
 }
 
+//grab shift amount
 bitset<5> Instruction:: shamt()
 {
   bitset<5> temp;
@@ -62,6 +76,7 @@ bitset<5> Instruction:: shamt()
   return temp;
 }
 
+//grab function
 bitset<6> Instruction:: funct()
 {
   bitset<6> temp;
@@ -74,9 +89,10 @@ bitset<6> Instruction:: funct()
   return temp;
 }
 
+//grab immediate value 
 bitset<16> Instruction::imm()
 {
-  //15-0                                                                                                                                                                                              
+  //15-0
   bitset<16> temp;
   for ( int i = 15; i >= 0; i--){
     temp[i] = bits[i];
@@ -84,9 +100,10 @@ bitset<16> Instruction::imm()
   return temp;
 }
 
+//grab jump offset
 bitset<26> Instruction::jmp_offset()
 {
-  //26-0                                                                                                                                                                                              
+  //26-0
   bitset<26> temp;
   int temp_count = 25;
   for( int i = 25; i >= 0; i--){
@@ -96,9 +113,12 @@ bitset<26> Instruction::jmp_offset()
   return temp;
 }
 
+//to_string function that converts given instruction to readable format
+//returns INVALID STRING if instruction not recognized
 string Instruction:: to_string()
 {
-  string final;
+  string final; // final opput string 
+  //grab each portion of bits for the final instruction
   bitset<6> opcode_ = this->opcode();
   bitset<5> rs_ = this->rs();
   bitset<5> rt_ = this->rt();
@@ -107,29 +127,91 @@ string Instruction:: to_string()
   bitset<6> func_ = this->funct();
   bitset<26> jump_address = this->jmp_offset();
   bitset<16> imm_ = this->imm();
-  //comparisons                                                                                                                                                                                       
+  //comparison variables
   bitset<6> zero(0);
   bitset<6> J_(2);
   bitset<6> JAL_(3);
+  bitset<6> SLL_(0);
+  bitset<6> SRL_(2);
+  //invalid string 
+  string invalid = "INVALID OPCODE";
+
+  //check to see if opcode is zero
+  //if zero then it should be R format so use function rather than opcode
   if (opcode_ == zero){
-    //R-Format func $rd,$rs,$rt                                                                                                                                                                       
-    final.append(r_functions[func_.to_ulong()]);
-    final.append(" ");
-    final.append(reg_names[rd_.to_ulong()]);
-    final.append(", ");
-    final.append(reg_names[rs_.to_ulong()]);
-    final.append(", ");
-    final.append(reg_names[rt_.to_ulong()]);
-  }//end if                                                                                                                                                                                           
+    //check if the function exists 
+    bool exit = false;
+    int count = 0;
+    while ( exit == false){
+      if( r_functions[func_.to_ulong()] == r_functions[count] ){
+	if ( r_functions[count] == "\0" ){
+	  //function does not exist
+	  return invalid;}
+	//function exists move on to R format
+	exit = true;
+      }//end if 
+      else{
+	count++;
+	if ( count == 44){
+	  //end of list function does not exist
+	  return invalid;
+	  exit = true;
+	}//end if
+      }//end if
+    }//end while
+	  
+    //R-Format func $rd,$rs,$rt
+    //check for SLL or SRL becuase they use
+    // func $rd,$rs,shamt instead
+    if ( func_ == SLL_ || func_ == SRL_ ){
+      final.append(r_functions[func_.to_ulong()]);
+      final.append(" ");
+      final.append(reg_names[rd_.to_ulong()]);
+      final.append(", ");
+      final.append(reg_names[rs_.to_ulong()]);
+      final.append(", ");
+      string shift = std::to_string(shamt_.to_ulong());
+      final.append(shift);
+    }//end if
+    else{
+      //not SLL or SRL 
+      final.append(r_functions[func_.to_ulong()]);
+      final.append(" ");
+      final.append(reg_names[rd_.to_ulong()]);
+      final.append(", ");
+      final.append(reg_names[rs_.to_ulong()]);
+      final.append(", ");
+      final.append(reg_names[rt_.to_ulong()]);
+    }//end else
+  }//end if
   else{
+    //check opcode to see if it exists
+    bool exit = false;
+    int count = 0;
+    while ( exit == false){
+      if( opcodes[opcode_.to_ulong()] == opcodes[count] ){
+	if ( opcodes[count] == "\0" ){
+	  return invalid;}
+	exit = true;
+      }//end if
+      else{
+	count++;
+	if ( count == 44){
+	  return invalid;
+	  exit = true;
+	}//end if
+      }//end else
+    }//end while
+    
+    //check for to J-format instructions
     if ( opcode_ == J_ || opcode_ == JAL_ ){
-      //J-Format op jmpadd                                                                                                                                                                            
+      //J-Format op jmpadd
       final.append(opcodes[opcode_.to_ulong()]);
       final.append(" ");
       final.append(jump_address.to_string());
-    }//end if                                                                                                                                                                                         
+    }//end if
     else{
-      //I-Format op $rt,$rs,imm                                                                                                                                                                       
+      //I-Format op $rt,$rs,imm
       final.append(opcodes[opcode_.to_ulong()]);
       final.append(" ");
       final.append(reg_names[rt_.to_ulong()]);
@@ -137,8 +219,8 @@ string Instruction:: to_string()
       final.append(reg_names[rs_.to_ulong()]);
       final.append(", ");
       final.append(imm_.to_string());
-    }//end else                                                                                                                                                                                       
-  }//end else                                                                                                                                                                                         
+    }//end else
+  }//end else
+  //return desired translated instruction
   return final;
 }
-
